28b9aa5ffaa093d1f00b31f3248e2be55fa3f671
cmevans5 <122652748+cmevans5@users.noreply.github.com>
2026-07-30 14:05:10 -0400

    Add scheduling package exports

---
+++ blue_battlefield/__init__.py
@@ -0,0 +1,25 @@
+"""Blue Battlefield attendance scheduling engine."""
+
+from .scheduler import (
+    Assignment,
+    AttendanceLedger,
+    Event,
+    Member,
+    Role,
+    ScheduleResult,
+    Scheduler,
+    Ship,
+    Signup,
+)
+
+__all__ = [
+    "Assignment",
+    "AttendanceLedger",
+    "Event",
+    "Member",
+    "Role",
+    "ScheduleResult",
+    "Scheduler",
+    "Ship",
+    "Signup",
+]
